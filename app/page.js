ff1fc3d93087c49b1ac3441603c7eebfff202695
mtabaie <mtabaie@gmail.com>
2024-02-17 18:55:59 +0330

    update

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+    <h1>Next - Redux ToolKit</h1>
+    </main>
+  );
+}
---
+++ redux/features/counter/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  counterValue: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.counterValue++;
+    },
+    decrement: (state) => {
+      state.counterValue--;
+    },
+    incrementByAmount: (state, action) => {
+      state.counterValue + -action.payload;
+    },
+  },
+});
+
+export default counterSlice.reducer;
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const selectCounter = (store) => store.counter.counterValue;
